57d7739ce55fd7243d87179c555e3700e52422c4
sinedied <noda@free.fr>
2020-05-11 17:35:11 +0200

    feat: add dynamic image

--- app/app.js
+++ app/app.js
@@ -1,11 +1,17 @@
 async function displayQuote() {
   let quote;
+
   try {
     const response = await fetch('/api/quote');
     quote = await response.json();
   } catch (error) {
     quote = { text: `Could not get quote: ${error.message}` };
   }
+
+  // Update background image
+  document.body.style.backgroundImage = `url(${quote.image})`;
+
+  // Update text
   const div = document.getElementById('quote');
   div.textContent = quote.text;
 }
