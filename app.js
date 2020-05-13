457f8ff1a8c0eb40aae2f6a3de1ac37431c67c79
sinedied <noda@free.fr>
2020-05-13 12:54:41 +0200

    feat: preload image before display

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ async function displayQuote() {
   let quote;
 
   try {
-    const response = await fetch('/api/quote');
+    // Fetch quote object from API
+    let response = await fetch('/api/quote');
     quote = await response.json();
+
+    // Preload image
+    response = await fetch(quote.image);
+    const image = await response.blob()
+    quote.image = URL.createObjectURL(image);
   } catch (error) {
     quote = { text: `Could not get quote: ${error.message}` };
   }
 
   // Update background image
-  document.body.style.backgroundImage = `url(${quote.image})`;
+  document.body.style.background = quote.image ? `url(${quote.image}) center / cover` : '#f43';
 
   // Update text
   const div = document.getElementById('quote');
